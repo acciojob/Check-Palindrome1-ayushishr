7c403a701c7f6495415f09182d2d80de682dcd93
ayushishr <128039555+ayushishr@users.noreply.github.com>
2023-06-04 13:14:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,18 @@
 
 function palindrome(str){
 
+	str = str.toLowerCase();
+	let string = str.split(" ").join('')
+	let arr=string.split('');
+	let i=0;j=arr.length-1;
+
+	while(i<j){
+		if(arr[i] !== arr[j]){
+			return false;
+		}
+		i++;
+		j--;
+	}
+	return true;
 }
 module.exports = palindrome
